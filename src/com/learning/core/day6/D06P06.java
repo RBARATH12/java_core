50cc18c97c0fbc34c78ce8a96a40baf5b64743ab
RBARATH12 <161832800+RBARATH12@users.noreply.github.com>
2024-03-11 17:31:18 +0530

    day6

---
+++ src/com/learning/core/day6/D06P06.java
@@ -0,0 +1,54 @@
+package com.learning.core.day6;
+
+import java.util.*;
+
+class Car implements Comparable<Car> {
+    private String name;
+    private double price;
+
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": $" + price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Car car = (Car) obj;
+        return Double.compare(car.price, price) == 0 && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return Double.compare(o.price, this.price); 
+}
+
+public class D06P06 {
+    public static void main(String[] args) {
+        
+        TreeMap<Car, String> carDetails = new TreeMap<>();
+
+       
+        carDetails.put(new Car("Bugatti", 80050.0), "1");
+        carDetails.put(new Car("Swift", 305000.0), "2");
+        carDetails.put(new Car("Audi", 600100.0), "3");
+        carDetails.put(new Car("Benz", 900000.0), "4");
+
+       
+        System.out.println("Car Prices in Reverse Order:");
+        for (Car car : carDetails.keySet()) {
+            System.out.println(car);
+        }
+    }
+}
